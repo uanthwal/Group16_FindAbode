177dc95ada14373aaf9bbab9684dd39a72c784cf
Ruize Nie <rz376775@dal.ca>
2020-06-20 19:24:26 -0300

    Project: Deploy project to heroku

---
+++ build/precache-manifest.ea01bbfa4991d52d5c54300eeec9c10d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6115a378200c64a30dc48faf8d65af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98670a978cf7e19014d3",
+    "url": "/static/css/main.27f93c66.chunk.css"
+  },
+  {
+    "revision": "886d9ee9a1f4dabfea77",
+    "url": "/static/js/2.466ce3b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.466ce3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98670a978cf7e19014d3",
+    "url": "/static/js/main.1e2ca6ce.chunk.js"
+  },
+  {
+    "revision": "5e2ab06eb8e92c057a8f",
+    "url": "/static/js/runtime-main.69a0ef0c.js"
+  },
+  {
+    "revision": "05980418b8dbda5eb0d4a135d0f46420",
+    "url": "/static/media/ad.05980418.jpg"
+  },
+  {
+    "revision": "30045daa36f4bc7bb6f54724bd6cb446",
+    "url": "/static/media/launch.30045daa.png"
+  }
+]);
