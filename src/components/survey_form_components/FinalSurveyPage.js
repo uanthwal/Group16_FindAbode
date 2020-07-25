105f8a76f37796e4f861e7e93ef441ffa258302c
pjain <prerna.jain24@gmail.com>
2020-07-25 15:27:27 -0300

    Added survey questions

---
+++ src/components/survey_form_components/FinalSurveyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../css/survey/SurveyFinalPage.css'
+const finalPage = () => {
+    return(
+        <div className = "final-outer-box" style = {{color: "black"}}>
+            <p style = {{color: "black", fontSize: "20px"}}><center>Thank you for letting us know about your experience so far. Your answers will help us make FindAbode even better for you and other guests.</center></p>
+            <p style = {{color: "black", fontSize: "18px"}}><center>Thanks, The FindAbode Team.</center></p>
+        </div>
+    )
+}
+export default finalPage;
---
+++ src/components/survey_form_components/SurveyForm.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import Axios from "axios";
+import "../../css/survey/Survey.css";
+import { APP_URL_CONFIG } from "../../App.Urls";
+import Finalpage from "./FinalSurveyPage";
+
+class Survey extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            questions: [{
+                question: "",
+                choice1: "",
+                choice2: "",
+                choice3: "",
+            }],
+            i: 0,
+            selectedOption: ""
+        };
+        this.onValueChange = this.onValueChange.bind(this);
+        this.formSubmit = this.formSubmit.bind(this);
+    }
+
+    componentWillMount() {
+        Axios.get(APP_URL_CONFIG.BASE_URL + APP_URL_CONFIG.SURVEY_QUESTION)
+            .then(res => {
+                this.setState({
+                    questions: res.data
+                })
+                console.log(res.data)
+                console.log("data is:" + this.state.questions)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    onValueChange(event) {
+        this.setState({
+            selectedOption: event.target.value,
+        });
+    }
+
+    formSubmit(event) {
+        event.preventDefault();
+        let k = this.state.i
+        Axios.post(APP_URL_CONFIG.BASE_URL + APP_URL_CONFIG.SURVEY_ANSWER, {
+            question: this.state.questions[this.state.i].question,
+            response: this.state.selectedOption
+        })
+        this.setState({
+            i: k + 1,
+            selectedOption : ""
+        })
+    }
+
+    render() {
+        return (
+            <div className="outer-form" >
+                {this.state.i < this.state.questions.length ? 
+                <div>
+                <label style = {{color: "black", fontSize: "22px"}}><center>Feedback Questions</center></label>
+                <div className = "question-box" >
+                    <form onSubmit={this.formSubmit}>
+                    
+                        <label style = {{fontSize: "18px"}}>{this.state.i + 1}. {this.state.questions[this.state.i].question}</label>
+                        {/* {this.state.selectedOption = ""} */}
+                        <div className="radio">
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={this.state.questions[this.state.i].choice1}
+                                    checked={this.state.selectedOption === this.state.questions[this.state.i].choice1}
+                                    onChange={this.onValueChange}
+                                />
+                                  {this.state.questions[this.state.i].choice1}
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={this.state.questions[this.state.i].choice2}
+                                    checked={this.state.selectedOption === this.state.questions[this.state.i].choice2}
+                                    onChange={this.onValueChange}
+                                />
+                                  {this.state.questions[this.state.i].choice2}
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={this.state.questions[this.state.i].choice3}
+                                    checked={this.state.selectedOption === this.state.questions[this.state.i].choice3}
+                                    onChange={this.onValueChange}
+                                />
+                                  {this.state.questions[this.state.i].choice3}
+                            </label>
+                        </div>
+                        {this.state.selectedOption != "" ? <button className="button-question" type="submit">
+                            Next
+                        </button> : <button className="button-question" type="submit" disabled>
+                            Next
+                        </button>}
+                        
+
+                    </form>
+                    </div>
+                </div> : <Finalpage/>}
+                
+            </div>
+
+        );
+    }
+        
+}
+export default Survey;
