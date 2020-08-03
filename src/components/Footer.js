eac4f154da2c8d89f83377b58322ef0bd4a5fba0
Ruminder Singh <rm868837@dal.ca>
2020-08-03 08:37:15 -0300

    Career link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,55 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "../css/Footer.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/Footer.css';
 
 class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="footer-inner">
-          <div>
-            <i className="fas fa-globe fa-2x" /> English (Canada)
-          </div>
-          <ul>
-            <li>
-              <Link to="/contact" className="footer-link">
-                Contact FindAbode
-              </Link>
-            </li>
-            <li>
-              <Link to="/faq" className="footer-link">
-                FAQ
-              </Link>
-            </li>
-            <li>
-              <Link to="/blogs" className="footer-link">
-                Blogs
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="footer-link">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/survey" className="footer-link">
-                Survey
-              </Link>
-            </li>
-            <li>
-              <Link to="/discussionforum1" className="footer-link">
-                Discussion Forum
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    );
-  }
+	render() {
+		return (
+			<footer className="footer">
+				<div className="footer-inner">
+					<div>
+						<i className="fas fa-globe fa-2x" /> English (Canada)
+					</div>
+					<ul>
+						<li>
+							<Link to="/contact" className="footer-link">
+								Contact FindAbode
+							</Link>
+						</li>
+						<li>
+							<Link to="/careers" className="footer-link">
+								Careers
+							</Link>
+						</li>
+						<li>
+							<Link to="/faq" className="footer-link">
+								FAQ
+							</Link>
+						</li>
+						<li>
+							<Link to="/blogs" className="footer-link">
+								Blogs
+							</Link>
+						</li>
+						<li>
+							<Link to="/about" className="footer-link">
+								About
+							</Link>
+						</li>
+						<li>
+							<Link to="/survey" className="footer-link">
+								Survey
+							</Link>
+						</li>
+						<li>
+							<Link to="/discussionforum1" className="footer-link">
+								Discussion Forum
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</footer>
+		);
+	}
 }
 export default Footer;
