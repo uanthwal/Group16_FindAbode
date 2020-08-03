9eef63d9941158f56740ad490e8ad0ffd515a46a
Ruminder Singh <rm868837@dal.ca>
2020-08-03 12:41:23 -0300

    Security check added

--- src/components/rating_component/Rating.jsx
+++ src/components/rating_component/Rating.jsx
@@ -11,17 +11,18 @@ import '../../css/rating/rating.css';
 export default class Rating extends Component {
 	state = {
 		applicationSubmitted: false
-	}
+	};
 	handleSubmit = async (event) => {
 		event.preventDefault();
 		console.log('Checking');
 		const data = new FormData(event.target);
 		const query = new URLSearchParams(this.props.location.search);
 		let apartmentId = data.get('apartment_id');
-		if (query.get('apartment_id') != null) {
-			apartmentId = query.get('apartment_id');
+		if (query.get('apartment_id') == null) {
+			alert('You are not eligible to fill this survey or kindly check the link sent to you');
+			return;
 		}
-		alert(apartmentId);
+
 		await axios
 			.post(APP_URL_CONFIG.BASE_URL + APP_URL_CONFIG.SAVE_RATING, {
 				cleanliness: data.get('cleanliness'),
@@ -29,14 +30,14 @@ export default class Rating extends Component {
 				communication: data.get('communication'),
 				comment: data.get('comment'),
 				accuracy: data.get('accuracy'),
-				location: data.get('location'),
+				location: data.get('location')
 			})
 			.then((response) => {
 				console.log(response.status);
 				if (response.status === 200) {
 					this.setState({ applicationSubmitted: true });
 				} else {
-					alert("Error occured. Please try again");
+					alert('Error occured. Please try again');
 				}
 				console.log('Saved');
 			});
@@ -44,75 +45,74 @@ export default class Rating extends Component {
 	render() {
 		let ratingView;
 		if (this.state.applicationSubmitted) {
-			ratingView = <h3>Form submitted succesfully</h3>
+			ratingView = <h3>Form submitted succesfully</h3>;
 		} else {
-			ratingView = <form onSubmit={this.handleSubmit}>
-						<h3>Rate the visited property</h3>
-						<p>
-							Kindly rate the property on different parameters 1 being lowest rating and 5 is maximum
-							rating
-						</p>
-						<input type="hidden" id="apartment_id" name="apartment_id" value='5f162e2294e0f6eff8e61da9'/>
-						<div class="form-group">
-							<label>Cleaniness</label>
-							<select className="form-control" id="cleanliness" name="cleanliness" required>
-								<option value="1">1</option>
-								<option value="2">2</option>
-								<option value="3">3</option>
-								<option value="4">4</option>
-								<option selected="selected" value="5">
-									5
-								</option>
-							</select>
-						</div>
-						<div class="form-group">
-							<label>Communication</label>
-							<select className="form-control" id="communication" name="communication" required>
-								<option value="1">1</option>
-								<option value="2">2</option>
-								<option value="3">3</option>
-								<option value="4">4</option>
-								<option selected="selected" value="5">
-									5
-								</option>
-							</select>
-						</div>
-						<div class="form-group">
-							<label>Location</label>
-							<select className="form-control" id="location" name="location" required>
-								<option value="1">1</option>
-								<option value="2">2</option>
-								<option value="3">3</option>
-								<option value="4">4</option>
-								<option selected="selected" value="5">
-									5
-								</option>
-							</select>
-						</div>
-						<div class="form-group">
-							<label>Accuracy of data posted on the post</label>
-							<select className="form-control" id="accuracy" name="accuracy" required>
-								<option value="1">1</option>
-								<option value="2">2</option>
-								<option value="3">3</option>
-								<option value="4">4</option>
-								<option selected="selected" value="5">
-									5
-								</option>
-							</select>
-						</div>
-						<div class="form-group">
-							<label>Comments you want to post on this property page on FindAbode</label>
-							<textarea class="form-control" id="comment" name="comment" rows="3" />
-						</div>
-						<button>Submit</button>
-					</form>
+			ratingView = (
+				<form onSubmit={this.handleSubmit}>
+					<h3>Rate the visited property</h3>
+					<p>
+						Kindly rate the property on different parameters 1 being lowest rating and 5 is maximum rating
+					</p>
+					<input type="hidden" id="apartment_id" name="apartment_id" value="5f162e2294e0f6eff8e61da9" />
+					<div class="form-group">
+						<label>Cleaniness</label>
+						<select className="form-control" id="cleanliness" name="cleanliness" required>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option selected="selected" value="5">
+								5
+							</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Communication</label>
+						<select className="form-control" id="communication" name="communication" required>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option selected="selected" value="5">
+								5
+							</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Location</label>
+						<select className="form-control" id="location" name="location" required>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option selected="selected" value="5">
+								5
+							</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Accuracy of data posted on the post</label>
+						<select className="form-control" id="accuracy" name="accuracy" required>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option selected="selected" value="5">
+								5
+							</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Comments you want to post on this property page on FindAbode</label>
+						<textarea class="form-control" id="comment" name="comment" rows="3" />
+					</div>
+					<button>Submit</button>
+				</form>
+			);
 		}
 		return (
 			<div className="ratingPage">
-				<div className="inner">
-					{ratingView}
-				</div>
+				<div className="inner">{ratingView}</div>
 			</div>
 		);
 	}
